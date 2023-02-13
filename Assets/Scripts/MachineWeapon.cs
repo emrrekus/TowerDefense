87483b117f0224f759252b0ed3d6866c368504f6
Emre <112270755+emrrekus@users.noreply.github.com>
2023-02-13 18:38:40 +0300

    a

--- Assets/Scripts/MachineWeapon.cs
+++ Assets/Scripts/MachineWeapon.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,7 +8,7 @@ public class MachineWeapon : MonoBehaviour
 {
    private Transform _TargetEnemyPosition;
    private Enemy _TargetEnemy;
-   private string _TargetTag;
+   public string _TargetTag;
    [SerializeField] private GameObject _mainObject;
 
    [Header("Health Settings")] 
@@ -15,13 +16,57 @@ public class MachineWeapon : MonoBehaviour
    public float _currentHealth;
    public Image _healthBar;
    public GameObject _damageEffect;
-   public GameObject _Radar;
+   
    private bool _isDead = false;
    
    [Header("Shots Settings")]
+   public GameObject _Radar;
    public float _Range = 15f;
    public float _fireRate = 1f;
    public float _rotationSpeed = 10f;
+   public Transform _returnObject;
+   private float _fireTime = 0f;
+   private Vector3 _radarScale;
+
+   private GameObject[] _Enemys;
 
 
+   IEnumerator CheckTheEnvironment()
+   {
+      while (true)
+      {
+         yield return new WaitForSeconds(.5f);
+
+         _Enemys = GameObject.FindGameObjectsWithTag(_TargetTag);
+
+         float ShortDistance = Mathf.Infinity;
+         Enemy TheNearestEnemy = null;
+
+         foreach (GameObject Enemy in _Enemys)
+         {
+            float DistanceToEnemy = Vector3.Distance(transform.position, Enemy.transform.position);
+
+            if (DistanceToEnemy < ShortDistance)
+            {
+               ShortDistance = DistanceToEnemy;
+               TheNearestEnemy = Enemy.GetComponent<Enemy>();
+            }
+
+            if (ShortDistance != null && ShortDistance <= _Range)
+            {
+               _TargetEnemyPosition = TheNearestEnemy.transform;
+               _TargetEnemy = TheNearestEnemy;
+               
+            }
+            else
+            {
+               _TargetEnemyPosition = null;
+            }
+         }
+
+      }
+
+     
+   }
+   
 }
